73e73da0b39b6ef9f6844f7bbaecc74982a87a92
AlecMenke <Alec.Menke@gmail.com>
2019-08-26 05:41:21 -0400

    improved readability. flexible spacing

--- start.js
+++ start.js
@@ -8,23 +8,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const askForGESearch = () =>
-  rl.question("Search GE: ", async itemQuery => {
-    //rerun if empty
-    if (itemQuery.trim().length === 0) return askForGESearch();
-    process.stdout.write("\x1b[36mSearching....\x1b[0m ");
-    const possibleItems = api.search(itemQuery);
+rl.question("Search GE: ", handleGEQuery);
 
-    if (possibleItems.length === 0) {
-      console.log("( \x1b[31mno results found\x1b[0m )");
-    } else {
-      const itemData = await api.lookupItem(possibleItems[0].id);
-      printResults(possibleItems, itemData);
-    }
+async function handleGEQuery(itemQuery) {
+  //rerun if empty
+  if (itemQuery.trim().length === 0) {
+    return rl.question("SearchGE: ", handleGEQuery);
+  }
 
-    askForGESearch();
-  });
-askForGESearch();
+  process.stdout.write("\x1b[36mSearching....\x1b[0m ");
+
+  const possibleItems = api.search(itemQuery);
+
+  if (possibleItems.length === 0) {
+    console.log("( \x1b[31mno results found\x1b[0m )");
+  } else {
+    const itemData = await api.lookupItem(possibleItems[0].id);
+    printResults(possibleItems, itemData);
+  }
+
+  //resets
+  rl.question("SearchGE: ", handleGEQuery);
+}
 
 function printResults(searchResults, selectedItemData) {
   const { item } = selectedItemData;
@@ -37,14 +42,9 @@ function printResults(searchResults, selectedItemData) {
 
   searchResults[0].name = item.name = "\033[1m" + item.name + "\x1b[0m";
 
-  const resultDetails = `
->    \x1b[33m${item.current.price}gp\x1b[0m per ${item.name}
->    ${item.day30.trend} trend of ${trendColor[item.day30.trend]}${
-    item.day30.change
-  }\x1b[0m over the last 30 days`;
-  console.log(
-    `
 
+  const searchView =
+    `
 
   [ Search Results ]
 ----------------------
@@ -52,11 +52,15 @@ ${searchResults
   .slice(0, 6)
   .map((res, i) => (i === 0 ? `   -> ${res.name}` : `    - ${res.name}`) + "\n")
   .join("")}` +
-      `${
-        searchResults.length > 7
-          ? `\n... ${searchResults.length - 7} more results\n\n`
-          : "\n"
-      }` +
-      resultDetails
-  );
+    `${
+      searchResults.length > 7
+        ? `\n... ${searchResults.length - 7} more results\n`
+        : ""
+    }
+    
+>    \x1b[33m${item.current.price}gp\x1b[0m per ${item.name}
+>    ${item.day30.trend} trend of ${trendColor[item.day30.trend]}${item.day30.change}\x1b[0m over the last 30 days
+`;
+
+  console.log(searchView);
 }
